ca4cd30009bf34c05e0da7e17753a66cc6aa6034
Felix RJ <shinwoi19@gmail.com>
2026-09-02 21:18:20 +0700

    Add: Cooldown for the reveal

--- Script/Start.cs
+++ Script/Start.cs
@@ -1,34 +1,92 @@
 using Godot;
 using System;
+using System.Collections.Generic;
 
 public partial class Start : Node2D
 {
 	// this is a stest for a github
 	[Export] private Label decision;
-	// Called when the node enters the scene tree for the first time.
+	[Export] private Timer timer;
+	[Export] private Timer timer2;
+	public bool timeoutvar = true;
+	private bool judgeholder = false;
+	private int num1 = 0;
+	private int num2 = -1;
+	private Godot.Collections.Array<string> liststring = new Godot.Collections.Array<string>();
 	public override void _Ready()
 	{
 		GameMechanics.instance.textchanger += Ontextchanger;
 		GameMechanics.instance.larperchecker();	
+		timer.Timeout += OnTimeout;
+		timer2.Timeout += OnTimeout2;
+		timeoutvar = false;
+		timer.Start();
+		timer2.Start();
+		// if (timeoutvar == false)
+		// {
+		// 	whiletimehas();
+		// }
+		liststring.Add("Larper");
+		liststring.Add("Real Fan");
 	}
 
 	public override void _ExitTree()
 	{
 		GameMechanics.instance.textchanger -= Ontextchanger;
+		timer.Timeout -= OnTimeout;
+		timer2.Timeout -= OnTimeout2;
+		num1 = 0;
+
 	}
+
+	private void OnTimeout()
+	{
+		timer.Stop();
+		timer2.Stop();
+		timeoutvar = true;
+		Ontextchanger(judgeholder);
+	}
+
+	private void OnTimeout2()
+	{
+		num1 += 1;
+			if (num1 % 2 == 0)
+			{
+				decision.Text = "";
+				decision.Text = liststring[0];
+			}
+			else
+			{
+				decision.Text = "";
+				decision.Text = liststring[1];
+			}
+	}
+
+	// private void whiletimehas()
+	// {
+	// 	for (int i = 0; timeoutvar == true; i*=-1)
+	// 	{
+	// 		GD.Print(i+(i*i));
+	// 	}
+	// }
 
 	public void Ontextchanger(bool judge)
 	{
-		if (judge == true)
+		judgeholder = judge;
+		if (timeoutvar == true)
 		{
-			GD.Print("larper here");
-			decision.Text = "Larper";
+			if (judge == true)
+			{
+				GD.Print("larper here");
+				decision.Text = "Larper";
+			}
+			else
+			{
+				GD.Print("real fan here");
+				decision.Text = "Real Fan";
+			}
 		}
-		else
-		{
-			GD.Print("real fan here");
-			decision.Text = "Real Fan";
-		}
+		
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
